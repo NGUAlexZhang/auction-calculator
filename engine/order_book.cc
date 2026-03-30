30f67352a133f308f4cc82cfc7439e71192cc112
ZhangZhe <alexzhangaccn@gmail.com>
2026-03-30 20:34:50 +0800

    feat: add order book

--- engine/order_book.cc
+++ engine/order_book.cc
@@ -1,10 +1,82 @@
 #include "order_book.h"
 
-void OrderBook::add_order(const Order& order) {
-    if (order.side == 1) {
-        this->buy_orders[order.price].push_back(order);
-    } else {
-        this->sell_orders[order.price].push_back(order);
+OrderBook::OrderBook(const OrderBook& other) {
+    // Deep copy of buy_orders
+    for (const auto& [price, orders] : other.buy_orders) {
+        for (const auto& order_ptr : orders) {
+            add_order(*order_ptr);
+        }
     }
-    this->order_id_map[order.order_id] = order;
+    // Deep copy of sell_orders
+    for (const auto& [price, orders] : other.sell_orders) {
+        for (const auto& order_ptr : orders) {
+            add_order(*order_ptr);
+        }
+    }
+}
+
+OrderBook::OrderBook(OrderBook&& other)
+            : buy_orders(std::move(other.buy_orders)),
+            sell_orders(std::move(other.sell_orders)),
+            id_map(std::move(other.id_map)) {}
+        
+OrderBook& OrderBook::operator=(const OrderBook& other) {
+    if (this == &other) {
+        return *this;
+    }
+    this->buy_orders.clear();
+    this->sell_orders.clear();
+    this->id_map.clear();
+
+    for (auto & [price, orders] : other.buy_orders) {
+        for (auto& order_ptr : orders) {
+            add_order(*order_ptr);
+        }
+    }
+    for (auto & [price, orders] : other.sell_orders) {
+        for (auto& order_ptr : orders) {
+            add_order(*order_ptr);
+        }
+    }
+
+    return *this;
+
+}
+
+OrderBook& OrderBook::operator=(OrderBook&& other) {
+    if (this == &other) {
+        return *this;
+    }
+    this->buy_orders = std::move(other.buy_orders);
+    this->sell_orders = std::move(other.sell_orders);
+    this->id_map = std::move(other.id_map);
+    return *this;
+}
+
+void OrderBook::add_order(const Order& order) {
+    auto it = id_map.find(order.order_id);
+    if (it == id_map.end()) {
+        throw std::runtime_error("Order ID already exists: " + std::to_string(order.order_id));
+    }
+    if (order.side == 1) {
+        buy_orders[order.price].push_back(std::make_unique<Order>(order));
+        id_map[order.order_id] = {order.price, true, std::prev(this->buy_orders[order.price].end())};
+    } else {
+        sell_orders[order.price].push_back(std::make_unique<Order>(order));
+        id_map[order.order_id] = {order.price, false, std::prev(this->sell_orders[order.price].end())};
+    }
+}
+
+void OrderBook::remove_order(uint64_t order_id) {
+    auto it = id_map.find(order_id);
+    if (it == id_map.end()) {
+        throw std::runtime_error("Order ID not found: " + std::to_string(order_id));
+    }
+    const auto& location = it->second;
+    if (location.is_buy) {
+        buy_orders[location.price].erase(location.it);
+    } else {
+        sell_orders[location.price].erase(location.it);
+    }
+    id_map.erase(it);
 }
--- engine/order_book.h
+++ engine/order_book.h
@@ -1,20 +1,29 @@
 #pragma once
 #include <map>
 #include <unordered_map>
+#include <memory>
+#include <list>
 #include "model/order.h"
 
 class OrderBook {
 public:
-    OrderBook() = default;
-    OrderBook(const OrderBook&);
-    OrderBook& operator=(const OrderBook&);
+    OrderBook() noexcept = default;
+    OrderBook(const OrderBook&) noexcept;
     OrderBook(OrderBook&&) noexcept;
+    OrderBook& operator=(const OrderBook&) noexcept;
     OrderBook& operator=(OrderBook&&) noexcept;
     void add_order(const Order& order);
+    void add_order(Order&& order);
     void remove_order(uint64_t order_id);
 
 private:
-    std::map<double, std::vector<Order>> buy_orders;  // Buy orders sorted
-    std::map<double, std::vector<Order>> sell_orders; // Sell orders sorted
-    std::unordered_map<uint64_t, Order> order_id_map; // Map for quick order lookup by order_id
+    using OrderPtr = std::unique_ptr<Order>;
+    struct OrderLocation {
+        double price; // Price level of the order
+        bool is_buy; // True if it's a buy order, false if it's a sell order
+        std::list<OrderPtr>::iterator it; // Iterator to the order in the list
+    };
+    std::map<double, std::list<OrderPtr>> buy_orders;  // Buy orders sorted
+    std::map<double, std::list<OrderPtr>> sell_orders; // Sell orders sorted
+    std::unordered_map<uint64_t, OrderLocation> id_map; // Map from order ID to order location
 };
