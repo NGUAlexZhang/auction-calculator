c5c44a14b113967d959aaa55eb27c8523fcd010f
ZhangZhe <alexzhangaccn@gmail.com>
2026-04-16 16:37:55 +0800

    feat: add trade test

--- model/trade.h
+++ model/trade.h
@@ -3,38 +3,58 @@
 #include <cstdint>
 #include <string>
 #include <iostream>
+#include <chrono>
 
-enum class TradeType {
-  NEW_ORDER,  // New order event
-  TRADE,      // Trade event
-  CANCEL      // Order cancellation event
-};
 
 struct Trade {
-  int64_t biz_index;  // Business index for the event
-  TradeType type;     // Type of the event (NEW_ORDER, TRADE, CANCEL)
-  uint64_t
-      order_id;  // Unique identifier for the order associated with the event
+  std::chrono::sys_time<std::chrono::milliseconds>
+      datetime;     // Date and time when the event occurred
+  std::string sym;  // Stock symbol
   double price;  // Price at which the event occurred (for NEW_ORDER and TRADE)
   uint64_t
       size;  // Number of shares involved in the event (for NEW_ORDER and TRADE)
-  int8_t side;  // 1 for buy, -1 for sell at Shanghai Stock Exchange, 3 for
-                // Borrow in, -3 for Borrow out at Shenzhen Stock Exchange (for
-                // NEW_ORDER and TRADE)
+
   uint64_t bid_order_id;  // Unique identifier for the bid order in a trade
                           // event (for TRADE)
   uint64_t
       ask_order_id;  // Unique identifier for the ask order in a trade event
+  uint64_t trade_id;  // Unique identifier for the trade event (for TRADE)
+  std::string exec_type;  // Execution type for the trade event (for TRADE)
+  std::string trade_bs_flag;  // Trade buy/sell flag for the trade event (for
+                              // TRADE)
+  uint32_t channel_no;  // Channel number through which the event was processed
+  int64_t biz_index;  // Business index for the event
 
   bool operator==(const Trade& other) const {
-    return this->biz_index == other.biz_index && this->type == other.type &&
-           this->order_id == other.order_id && this->price == other.price &&
-           this->size == other.size && this->side == other.side &&
-           this->bid_order_id == other.bid_order_id &&
-           this->ask_order_id == other.ask_order_id;
+    return this->trade_id == other.trade_id;
   }
-  bool operator==(const std::uint64_t order_id) const {
-    return this->order_id == order_id;
+  bool operator==(const std::uint64_t trade_id) const {
+    return this->trade_id == trade_id;
   }
 
 };
+
+std::istream& operator >> (std::istream& is, Trade& trade) {
+  auto next_value = [&is]() -> std::string {
+    std::string value;
+    if (!std::getline(is, value, ',')) {
+      throw std::runtime_error("Failed to parse line stream");
+    }
+    return value;
+  };
+  std::istringstream datetime_ss(next_value());
+  std::chrono::system_clock::time_point tp;
+  std::chrono::from_stream(datetime_ss, "%Y-%m-%d %H:%M:%S", tp);
+  trade.datetime = std::chrono::time_point_cast<std::chrono::milliseconds>(tp);
+  trade.sym = next_value();
+  trade.price = std::stod(next_value());
+  trade.size = std::stoull(next_value());
+  trade.bid_order_id = std::stoull(next_value());
+  trade.ask_order_id = std::stoull(next_value());
+  trade.trade_id = std::stoull(next_value());
+  trade.exec_type = next_value();
+  trade.trade_bs_flag = next_value();
+  trade.channel_no = std::stoul(next_value());
+  trade.biz_index = std::stoll(next_value());
+  return is;
+}
--- tests/data_test.cc
+++ tests/data_test.cc
@@ -5,6 +5,7 @@
 
 #include "data_simulator.h"
 #include "order.h"
+#include "trade.h"
 
 std::ostream& operator<<(std::ostream& os, const Order& order) {
   os << "Datetime: " << std::format("{:%Y/%m/%d %H:%M:%S}", order.datetime)
@@ -13,6 +14,19 @@ std::ostream& operator<<(std::ostream& os, const Order& order) {
      << ", Order Type: " << order.order_type << ", Order ID: " << order.order_id
      << ", Channel No: " << order.channel_no << ", Seq No: " << order.seq_no
      << ", Biz Index: " << order.biz_index;
+  return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Trade& trade) {
+  os << "Datetime: " << std::format("{:%Y/%m/%d %H:%M:%S}", trade.datetime)
+     << ", Symbol: " << trade.sym << ", Price: " << trade.price
+     << ", Size: " << trade.size << ", Bid Order ID: " << trade.bid_order_id
+     << ", Ask Order ID: " << trade.ask_order_id
+     << ", Trade ID: " << trade.trade_id
+     << ", Exec Type: " << trade.exec_type
+     << ", Trade BS Flag: " << trade.trade_bs_flag
+     << ", Channel No: " << trade.channel_no
+     << ", Biz Index: " << trade.biz_index;
   return os;
 }
 
@@ -42,12 +56,43 @@ TEST(OrderSimulatorTest, TestEndOfFile) {
 
   EXPECT_NO_THROW({
     DataSimulator<Order> simulator(test_data_dir);
-    while (simulator.has_next()) {
-      Order order;
-      simulator >> order;
+    Order order;
+    while (simulator >> order) {
     }
   });
 }
+
+TEST(TradeSimulatorTest, TestReadCSV) {
+  std::string test_data_dir = TEST_DATA_DIR "trade.csv";
+  EXPECT_NO_THROW({
+    DataSimulator<Trade> simulator(test_data_dir);
+    Trade trade;
+    simulator >> trade;
+    std::cout << trade << std::endl;
+    simulator >> trade;
+    std::cout << trade << std::endl;
+  });
+}
+
+TEST(TradeSimulatorTest, TestEndOfFile) {
+  std::string test_data_dir = TEST_DATA_DIR "trade.csv";
+  EXPECT_THROW(
+      {
+        DataSimulator<Trade> simulator(test_data_dir);
+        while (true) {
+          Trade trade;
+          simulator >> trade;
+        }
+      },
+      std::runtime_error);
+
+  EXPECT_NO_THROW(
+    {
+      DataSimulator<Trade> simulator(test_data_dir);
+      Trade trade;
+    }
+  );
+};
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
